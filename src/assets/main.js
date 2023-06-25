3c98aa31d78e6a3061f034769d615947776d341d
david torres <david.martinezezguerra@gmail.com>
2023-06-24 20:55:02 -0500

    agregando cursor y tipografia ya arreglendo el titulo

--- src/assets/main.js
+++ src/assets/main.js
@@ -32,7 +32,7 @@ async function fetchData(urlApi) {
           </h3>
         </div>
       </div>
-        `).slice(0, 4).join('')}`;
+        `).slice(0, 8).join('')}`;
         content.innerHTML = view;
     }
     catch (error) {
@@ -40,45 +40,3 @@ async function fetchData(urlApi) {
     }
 })();
 
-// const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
-
-// const content = null || document.getElementsByClassName('video-content');
-
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-//     'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
-//   }
-// };
-
-// async function fetchData(urlApi) {
-//   const response = await fetch(urlApi, options);
-//   const data = await response.json();
-//   return data;
-// }
-
-// (async () => {
-//   try {
-//     const videos = await fetchData(API);
-//     let view = `
-//     ${videos.items.map(video => `
-//       <div class="group">
-//         <div
-//           class="w-full">
-//           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}">
-//         </div>
-//         <div class="tittle-video">
-//           <h3>
-//             <span></span>
-//             ${video.snippet.title}
-//           </h3>
-//         </div>
-//       </div>
-//     `).slice(0, 4).join('')}
-//     `;
-//     content.innerHTML = view;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
